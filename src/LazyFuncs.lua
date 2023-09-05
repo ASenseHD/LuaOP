b4ab9ef0c262ae8d929a19748332981c00df1041
geometrical1 <geometricalalt@gmail.com>
2023-09-05 20:01:45 +0300

    Split code massively

---
+++ src/LazyFuncs.lua
@@ -0,0 +1,29 @@
+local LazyFuncs = {}
+function LazyFuncs:CheckVoid(...)
+	if select("#", ...) > 1 then
+		error("expected 1 argument for CheckVoid", 2)
+	end
+
+	return select("#", ...) == 0
+end
+
+function LazyFuncs:CheckArg(func, pos, arg, possibleArgType)
+	local currentArgType = type(arg)
+
+	if type(possibleArgType) == "table" then
+		for _, acceptableArgType in next, possibleArgType do
+			if acceptableArgType == currentArgType then
+				return
+			end
+		end
+
+		error(`invalid argument #{pos} to '{func}' (expected {table.concat(possibleArgType, " or ")}, got {currentArgType})`, 2)
+	elseif type(possibleArgType) == "string" then
+		if possibleArgType ~= currentArgType then
+			error(`invalid argument #{pos} to '{func}' (expected {possibleArgType}, got {currentArgType})`, 2)
+		end
+	else
+		error("expected a string for the type argument", 2)
+	end
+end
+return LazyFuncs
---
+++ src/init.lua
@@ -0,0 +1,5 @@
+local Constructor, LOPObject = {}, require(script.LOPObject)
+function Constructor.new()
+	return setmetatable({}, {__index = LOPObject})
+end
+return Constructor
